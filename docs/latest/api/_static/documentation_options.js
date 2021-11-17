97d74422b5ad15ea4f1d27ddb1a372d75d5698df
mysticfall <mysticfall@users.noreply.github.com>
2021-11-17 06:25:25 +0000

    deploy: 47f22c87df08420a641f7f034a9f40121fd70881

---
+++ docs/latest/api/_static/documentation_options.js
@@ -0,0 +1,1 @@
+var DOCUMENTATION_OPTIONS={URL_ROOT:document.getElementById("documentation_options").getAttribute("data-url_root"),VERSION:"870f39f58644",LANGUAGE:"None",COLLAPSE_INDEX:!1,BUILDER:"html",FILE_SUFFIX:".html",LINK_SUFFIX:".html",HAS_SOURCE:!1,SOURCELINK_SUFFIX:".txt",NAVIGATION_WITH_KEYS:!1};
